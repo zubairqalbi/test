c49aae54d9c5857d44c4fc48ee1ab39d82f5886c
unknown <mkzb3@mail.umkc.edu>
2018-10-22 18:53:46 -0500

    water

--- hungary.py
+++ hungary.py
@@ -4,4 +4,6 @@ if hungary == "yes":
     print ("eat pizza")
     print ("eat burger")
 else:
-    print ("do your work")
+    thirsty = input("are you thirsty?")
+    if thirsty == "yes":
+        print ("drink water")
