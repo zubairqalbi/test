c01b0a8a1a6c6aaf20dfd67f1dbd1f24949c0e7c
unknown <mkzb3@mail.umkc.edu>
2018-10-22 22:03:20 -0500

    doctor new version

--- doctor.py
+++ doctor.py
@@ -1,7 +1,7 @@
 doctor = input("are you doctor?")
 if doctor == "yes":
     print ("cure patient")
-
+    print ("give medicine")
 else:
     print ("you would be engineer")
     print ("call emergency")
