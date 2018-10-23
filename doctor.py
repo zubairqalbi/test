306f23c50d329dfd9e1c9fa85c380afb1b958cdc
unknown <mkzb3@mail.umkc.edu>
2018-10-23 03:37:51 -0500

    kindkhan

--- doctor.py
+++ doctor.py
@@ -1,20 +1,7 @@
 doctor = input("are you doctor?")
-if doctor == "yes":
+if doctor == "yes" and doctor =="browny":
     print ("cure patient")
     print ("give medicine")
     print ("also provide injection")
 else:
-    print ("you would be engineer")
-    print ("call emergency")
-
-print ("how are you guys")
-print ("i am from pakistan")
-print ("where are you from ?")
-
-print (" its late night")
-print ("feeling loneliness")
-
-print ("its time to pray")
-print ("lets gether in mosque")
-
-print ("what are you doing zubair")
+    print ("my name is khan and i am not a terrorist")
