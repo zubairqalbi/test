1eeaa4b287a13392947be3e677ab7b45bc67fe67
unknown <mkzb3@mail.umkc.edu>
2018-10-22 22:04:47 -0500

    injection

--- doctor.py
+++ doctor.py
@@ -2,6 +2,7 @@ doctor = input("are you doctor?")
 if doctor == "yes":
     print ("cure patient")
     print ("give medicine")
+    print ("also provide injection")
 else:
     print ("you would be engineer")
     print ("call emergency")
